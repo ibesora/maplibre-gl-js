c82cdea8c33b0cc85b966d54f47d81ccffb19539
Isaac Besora <isaac.besora@gmail.com>
2024-10-15 17:00:01 +0200

    Fix terrain fog

--- src/shaders/terrain.fragment.glsl
+++ src/shaders/terrain.fragment.glsl
@@ -20,7 +20,9 @@ vec4 linearToGamma(vec4 color) {
 
 void main() {
     vec4 surface_color = texture(u_texture, vec2(v_texture_pos.x, 1.0 - v_texture_pos.y));
-    if (v_fog_depth > u_fog_ground_blend) {
+    // v_fog_depth should be in the range [0, 1] but it can be outside of this range due to precision issues
+    float clamped_fog_depth = clamp(v_fog_depth, 0.0, 1.0);
+    if (clamped_fog_depth > u_fog_ground_blend) {
         vec4 surface_color_linear = gammaToLinear(surface_color);
         float blend_color = smoothstep(0.0, 1.0, max((v_fog_depth - u_horizon_fog_blend) / (1.0 - u_horizon_fog_blend), 0.0));
         vec4 fog_horizon_color_linear = mix(gammaToLinear(u_fog_color), gammaToLinear(u_horizon_color), blend_color);
